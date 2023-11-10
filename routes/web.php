67a88717e352d04ff6a0de300d5614dd5b92e447
Lajtaib0801 <lajtaibenja@gmail.com>
2023-11-10 12:32:58 +0100

    c

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\QueryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/actors', [QueryController::class, 'actors']);
